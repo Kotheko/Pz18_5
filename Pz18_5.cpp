853d299a0724913d1b90720c069eae20e154dcb8
Ruslan Denisov <necronlive@mail.ru>
2024-01-31 12:29:25 +0900

    Random added

--- Pz18_5.cpp
+++ Pz18_5.cpp
@@ -5,7 +5,10 @@ class Gamer
 {
 public:
     Gamer()
-    {}
+    {
+        GenerateName();
+        GenerateScore();
+    }
 
     Gamer(std::string _name, int _score) : name(_name), score(_score)
     {}
@@ -20,9 +23,63 @@ public:
         return score;
     }
 
+    //Сортировка выбором
+    static void SelectionSort(Gamer* l, Gamer* r)
+    {
+        for (Gamer* i = l; i < r; i++)
+        {
+            Gamer max = *i, * ind = i;
+            for (Gamer* j = i + 1; j < r; j++)
+            {
+                if (j->getScore() > max.getScore())
+                {
+                    max = *j, ind = j;
+                }
+            }
+            std::swap(*i, *ind);
+        }
+    }
+
+private:
+    void GenerateName()
+    {
+        int start = 1;
+        int end = names->length();
+
+        int index = std::rand() % (end - start + 1) + start;
+
+        name = names[index];
+    }
+
+    void GenerateScore()
+    {
+        int start = 1;
+        int end = 100;
+
+        score = std::rand() % (end - start + 1) + start;
+    }
+
 private:
     std::string name;
     int score;
+    std::string names[15] =
+    {
+        "Василий",
+        "Эдуард",
+        "Геннадий",
+        "Глафира",
+        "Елизавета",
+        "Анатолий",
+        "Анастасия",
+        "Вячеслав",
+        "Иннокентий",
+        "Ярослав",
+        "Елисей",
+        "Константин",
+        "Станислав",
+        "Екатерина",
+        "Татьяна"
+    };
 };
 
 int main()
@@ -37,16 +94,18 @@ int main()
 
     for (int i = 0; i < count; i++)
     {
-        std::string name;
-        int score;
+        //std::string name;
+        //int score;
 
-        std::cout << "Имя " + std::to_string(i + 1) + "-го игрока:\n";
-        std::cin >> name;
+        //std::cout << "Имя " + std::to_string(i + 1) + "-го игрока:\n";
+        //std::cin >> name;
 
-        std::cout << "Количество очков:\n";
-        std::cin >> score;
+        //std::cout << "Количество очков:\n";
+        //std::cin >> score;
 
-        arr[i] = Gamer(name, score);
+        //arr[i] = Gamer(name, score);
+
+        arr[i] = Gamer();
     }
     std::cout << "----------\n";
 
@@ -57,8 +116,7 @@ int main()
     std::cout << "----------\n";
 
     //Сортировка выбором (Selection sort)
-    Gamer tmp;
-    for (int i = 0; i < count - 1; i++)
+ /*   for (int i = 0; i < count - 1; i++)
     {
         int max = i;
 
@@ -70,13 +128,10 @@ int main()
             }
         }
 
-        if (i != max)
-        {
-            tmp = arr[i];
-            arr[i] = arr[max];
-            arr[max] = tmp;
-        }
-    }
+        std::swap(arr[i], arr[max]);
+    }*/
+
+    arr->SelectionSort(arr, arr + count);
 
     for (int i = 0; i < count; i++)
     {
